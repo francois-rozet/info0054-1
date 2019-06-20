72dbe55775ab271dc6ebcf1e6613f76d4f1bc066
François Rozet <francois.rozet@outlook.com>
2019-06-20 22:37:23 +0200

    Correction a posteriori

--- scheme/solver.scm
+++ scheme/solver.scm
@@ -5,21 +5,6 @@
 
 ;; UTILS
 
-;; Pour le comparateur comp, l'élément x et la liste l triée selon comp,
-;; insert retourne la liste triée l où a été inséré x.
-(define (insert x l comp)
-	(cond
-		((null? l) (list x))
-		((comp x (car l)) (cons x l))
-		(else
-			(cons
-				(car l)
-				(insert x (cdr l) comp)
-			)
-		)
-	)
-)
-
 ;; Pour le comparateur comp et les listes l et r triées selon comp,
 ;; merge retourne la liste triée des éléments de l et r réunis.
 (define (merge l r comp)
@@ -47,43 +32,20 @@
 	(< (car l) (car r))
 )
 
-;; Pour le prédicat unaire p? et la liste l,
-;; divide-filter retourne la paire dont
-;; le car est la liste des éléments de l pour lesquels p? est #t et
-;; le cdr la liste des autres éléments de l.
-(define (divide-filter p? l)
-	(divide-filter-acc p? l '() '())
-)
-
-;; Pour le prédicat unaire p? et les listes l, acc-t et acc-f,
-;; divide-filter-acc retourne la paire dont
-;; le car est (append "la liste des éléments de l pour lesquels p? est #t" acc-t) et
-;; le cdr (append "la liste des autres éléments de l" acc-f).
-(define (divide-filter-acc p? l acc-t acc-f)
-	(cond 
-		((null? l) (cons acc-t acc-f))
-		((p? (car l))
-			(divide-filter-acc p? (cdr l) (cons (car l) acc-t) acc-f)
-		)
-		(else
-			(divide-filter-acc p? (cdr l) acc-t (cons (car l) acc-f))
-		)
-	)
-)
-
 ;; SOLVER
 
-;; N.B. Un chemin est une paire dont le car est la liste de coups du dernier au premier joués et
-;; le cdr une paire dont le car est le dernier état visité et le cdr le set d'états intermédiaires.
+;; N.B. Un chemin est une structure de la forme (word . states) où
+;; word est la liste de coups du dernier au premier joués et
+;; states la liste des états visités du dernier au premier.
 
-;; Pour un chemin path, word retourne la liste de coups joués du premier au dernier.
-(define (word path) (reverse (car path)))
-
-;; Pour un chemin, last-state retourne le dernier état visité.
+(define word car)
+(define states cdr)
 (define last-state cadr)
 
-;; Pour un chemin, states retourne le set d'états intermédiaires.
-(define states cddr)
+;; Pour un état s, start initialise un chemin à l'état s.
+(define (start s)
+	(cons '() (list s))
+)
 
 ;; Pour un chemin path et une fonction d'adjacence adj,
 ;; child retourne la liste des chemins valides enfants de path.
@@ -91,64 +53,39 @@
 	(map
 		(lambda (x)
 			(cons
-				(cons (car x) (car path))
-				(cons (cdr x) (set-add (states path) (cadr path)))
+				(cons (car x) (word path))
+				(cons (cdr x) (states path))
 			)
 		)
 		(filter
-			(lambda (y) (not (set-member? (states path) (cdr y))))
+			(lambda (y) (not (member (cdr y) (states path))))
 			(adj (last-state path))
 		)
 	)
 )
 
-;; Pour les chemins l et r,
-;; shorter? retourne #t si l est plus court que r, #f sinon.
-(define (shorter? l r)
-	(<= (length (word l)) (length (word r)))
-)
-
 ;; Pour les fonctions d'adjacence et d'acceptation adj et acc-state?,
-;; la liste de chemins favoris queue,
-;; le set d'états présents dans les chemins de queue visited et
-;; la liste de chemins laissés de côtés waiting,
-;; fun est un itérateur paresseux d'un sous-ensemble du sous-language des mots sans cycle du puzzle.
+;; une liste de chemins de longueur N stack et
+;; une liste de chemins de longueur N + 1 acc,
+;; fun est un itérateur paresseux d'un sous-ensemble du sous-language
+;; des mots sans cycle du puzzle d'au moins N symboles.
 ;; Ces mots sont donnés par ordre croissant de longueur.
-(define (fun adj acc-state? visited queue waiting)
-	(if (null? queue)
-		'()
-		(let*
-			(
-				(path (car queue))
-				(q (last-state path))
+(define (fun adj acc-state? stack acc)
+	(cond 
+		((null? stack)
+			(if (null? acc)
+				'()
+				(fun adj acc-state? acc '())
 			)
-			(cond
-				((acc-state? q)
-					(let*
-						(
-							(temp
-								(divide-filter
-									(lambda (x) (set-member? (states path) (last-state x)))
-									waiting
-								)
-							)
-							(visited* (set-subtract visited (states path)))
-							(queue* (merge (sort (car temp) shorter?) (cdr queue) shorter?))
-							(waiting* (cdr temp))
-						)
-						(cons
-							(word path)
-							(lambda () (fun adj acc-state? visited* queue* waiting*))
-						)
-					)
-				)
-				((set-member? visited q)
-					(fun adj acc-state? visited (cdr queue) (cons path waiting))
-				)
-				(else
-					(fun adj acc-state? (set-add visited q) (merge (cdr queue) (child adj path) shorter?) waiting)
-				)
+		)
+		((acc-state? (last-state (car stack)))
+			(cons
+				(reverse (word (car stack)))
+				(lambda () (fun adj acc-state? (cdr stack) acc))
 			)
+		)
+		(else
+			(fun adj acc-state? (cdr stack) (append (child adj (car stack)) acc))
 		)
 	)
 )
@@ -158,8 +95,7 @@
 		(fun
 			adj
 			acc-state?
-			(set)
-			(list (cons '() (cons s (set))))
+			(list (start s))
 			'()
 		)
 	)
@@ -175,9 +111,9 @@
 (define (fun-heuristic adj acc-state? heuristic p-queue)
 	(cond
 		((null? p-queue) '())
-		((acc-state? (last-state (cdar p-queue)))
+		((zero? (caar p-queue))
 			(cons
-				(word (cdar p-queue))
+				(reverse (word (cdar p-queue)))
 				(lambda () (fun-heuristic adj acc-state? heuristic (cdr p-queue)))
 			)
 		)
@@ -189,10 +125,10 @@
 				(merge
 					(sort
 						(map
-							(lambda (path)
+							(lambda (x)
 								(cons
-									(heuristic (last-state path))
-									path
+									(heuristic (last-state x))
+									x
 								)
 							)
 							(child adj (cdar p-queue))
@@ -216,7 +152,7 @@
 			(list
 				(cons
 					(heuristic s)
-					(cons '() (cons s (set)))
+					(start s)
 				)
 			)
 		)
